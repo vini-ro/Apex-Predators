91bfb37da1cca64e56179d94cb39ab2c6a06d0f3
Vinicius <vroliveira.eng@gmail.com>
2024-08-21 17:17:23 -0300

    Filter & Sort

--- Apex Predators/ContentView.swift
+++ Apex Predators/ContentView.swift
@@ -10,16 +10,12 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     @State var searchText = ""
+    @State var alphabetical = false
     
     var filteredDinos: [ApexPredator] {
-        if searchText.isEmpty {
-            return predators.apexPredators
-        } else {
-            return predators.apexPredators.filter {
-                predator in
-                predator.name.localizedCaseInsensitiveContains(searchText)
-            }
-        }
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
     }
     
     var body: some View {
@@ -59,6 +55,25 @@ struct ContentView: View {
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                            
+                        }
+                        } label: {
+//                        if alphabetical {
+//                            Image(systemName: "film")
+//                        } else {
+//                            Image(systemName: "textformat")
+//                        }
+                            Image(systemName: alphabetical ? "film" : "textformat")
+                                .symbolEffect(.bounce, value: alphabetical)
+                            
+                    }
+                }
+            }
         }
         .preferredColorScheme(.dark)
         }
--- Apex Predators/Predators.swift
+++ Apex Predators/Predators.swift
@@ -27,4 +27,24 @@ class Predators {
             }
         }
     }
+    
+    func search(for searchTerm: String) -> [ApexPredator] {
+        if searchTerm.isEmpty {
+            return apexPredators
+        } else {
+            return apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool) {
+        apexPredators.sort { predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
+    }
 }
