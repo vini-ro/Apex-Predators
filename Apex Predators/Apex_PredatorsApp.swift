0e52803b5c678e0741fd704240173f42f128d187
Vinicius <vroliveira.eng@gmail.com>
2024-08-21 14:52:20 -0300

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by Vinicius Oliveira on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
